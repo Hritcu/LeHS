46d7ab175c8a0a0f5faa601365e9d72b83cfbde9
ROana-coder <59318326+ROana-coder@users.noreply.github.com>
2021-06-11 12:17:22 +0300

    Update index.php

--- proiect/index.php
+++ proiect/index.php
@@ -2,16 +2,16 @@
 
 
 
-// load the (optional) Composer auto-loader
+
 if (file_exists('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 }
 
 require 'application/config/config.php';
 
-// load application class
+
 require 'application/libs/application.php';
 require 'application/libs/controller.php';
 
-// start the application
+// start 
 $app = new Application();
