24585ef9d94209a8d006ac7bc27d9e1896f28c29
Ioana Tanasiciuc <75720980+ioanatanasiciuc@users.noreply.github.com>
2021-06-11 12:16:26 +0300

    final

--- proiect/application/controller/login.php
+++ proiect/application/controller/login.php
@@ -5,9 +5,7 @@ require "application/config/init.php";
 session_start();
 $_SESSION['username'] = "Ema Hritcu";
 $_SESSION['email'] = "hritcuema2000@gmail.com";
-//this will redirect user to github authorization page
+
 goToAuthUrl();
 
-
-//if no redirection occur then following shows.
 echo "operation failed.";
