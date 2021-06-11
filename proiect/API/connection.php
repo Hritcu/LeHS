d311423f77a17a8c165555d8a39d08679ef17546
ROana-coder <59318326+ROana-coder@users.noreply.github.com>
2021-06-11 12:16:12 +0300

    Update connection.php

--- proiect/API/connection.php
+++ proiect/API/connection.php
@@ -10,7 +10,7 @@ Class dbObj{
 	function getConnstring() {
 		$con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
 
-		/* check connection */
+		
 		if (mysqli_connect_errno()) {
 			printf("Connect failed: %s\n", mysqli_connect_error());
 			exit();
